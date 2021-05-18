1ca30a1bfdd2e2559f5953c44451aba3b3dacc2d
Sticcia <davidercosta@gmail.com>
2021-05-18 14:31:16 +0200

    Create project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace Fantamorto_Bot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string accessToken = "1558692324:AAE-SlPNiopanwBJBgSDBOEZYz9BqEqwGrQ";
+			var botClient = new TelegramBotClient(accessToken);
+			var me = botClient.GetMeAsync().Result;
+			Console.WriteLine($"Hello World! I am user {me.Id} and my name is {me.FirstName}.");
+		}
+	}
+}
