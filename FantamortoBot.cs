840a44b18d3c6f8704609f4f97a4db520f5c28e1
Sticcia <davidercosta@gmail.com>
2021-05-18 14:47:18 +0200

    Create FantamortoBot class

---
+++ FantamortoBot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace FantamortoBot
+{
+	class FantamortoBot
+	{
+		static ITelegramBotClient botClient;
+		public FantamortoBot(string accessToken)
+		{
+			botClient = new TelegramBotClient(accessToken);
+			botClient.OnMessage += Bot_OnMessage;
+		}
+
+		public void Start()
+		{
+			botClient.StartReceiving();
+		}
+
+		public void Stop()
+		{
+			botClient.StopReceiving();
+		}
+
+		public static async void Bot_OnMessage(object sender, MessageEventArgs e)
+		{
+			if (e.Message.Text != null)
+			{
+				Console.WriteLine($"Received message: {e.Message.Text} from chat: {e.Message.Chat.Id}");
+
+				await botClient.SendTextMessageAsync(e.Message.Chat, e.Message.Text);
+			}
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
-using Telegram.Bot;
 
-namespace Fantamorto_Bot
+namespace FantamortoBot
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
 			string accessToken = "1558692324:AAE-SlPNiopanwBJBgSDBOEZYz9BqEqwGrQ";
-			var botClient = new TelegramBotClient(accessToken);
-			var me = botClient.GetMeAsync().Result;
-			Console.WriteLine($"Hello World! I am user {me.Id} and my name is {me.FirstName}.");
+			var bot = new FantamortoBot(accessToken);
+
+			bot.Start();
+
+			Console.WriteLine("Press any key to exit...");
+			Console.ReadKey();
+
+			bot.Stop();
 		}
 	}
 }
