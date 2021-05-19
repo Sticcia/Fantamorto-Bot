c83df21569b63b342edc66ae3eb723d3644d922d
Sticcia <davidercosta@gmail.com>
2021-05-19 14:11:28 +0200

    Create CommandHandler class

---
+++ CommandHandler.cs
@@ -0,0 +1,90 @@
+﻿using System.IO;
+
+using Telegram.Bot.Args;
+
+namespace Fantamorto_Bot
+{
+	class CommandHandler
+	{
+		private readonly MessageEventArgs e;
+		private readonly string fileName;
+		private string response = "";
+
+		public CommandHandler(string filePath, MessageEventArgs messageEvent)
+		{
+			e = messageEvent;
+			fileName = filePath + e.Message.From.Id.ToString();
+			response = "";
+		}
+
+		public string GetResponse()
+		{
+			return response;
+		}
+
+		public void NewList()
+		{
+			if (File.Exists(fileName))
+			{
+				response = $"List for {e.Message.From.Id} already exists";
+				PrintList();
+			}
+			else
+			{
+				response = $"Created new list for {e.Message.From.FirstName}, use /add to add names";
+				using FileStream fs = File.Create(fileName);
+			}
+		}
+
+		public void PrintList()
+		{
+			if (File.Exists(fileName))
+			{
+				if (new FileInfo(fileName).Length == 0)
+				{
+					response = "Empty list, use /add to add names";
+				}
+				else
+				{
+					response = File.ReadAllText(fileName);
+				}
+			}
+			else
+			{
+				response = $"No list for {e.Message.From.FirstName} was found, use /new to create a new list";
+			}
+		}
+
+		public void AddName()
+		{
+			var args = e.Message.Text.Split(" ", 2);
+
+			if (File.Exists(fileName))
+			{
+				if (args.Length != 2)
+				{
+					response = "Argument missing, use /add <name>";
+				}
+				else
+				{
+					File.AppendAllTextAsync(fileName, $"{args[1]}\n");
+					response = $"Added {args[1]} to {fileName}";
+				}
+			}
+			else
+			{
+				response = $"No list for {e.Message.From.FirstName} was found, use /new to create a new list";
+			}
+		}
+
+		public void RemoveName()
+		{
+			response = "Command not implemented yet";
+		}
+
+		public void AddDead()
+		{
+			response = "Command not implemented yet";
+		}
+	}
+}
--- FantamortoBot.cs
+++ FantamortoBot.cs
@@ -1,25 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
+using System.Collections.Generic;
 
 using Telegram.Bot;
 using Telegram.Bot.Args;
+
+using Fantamorto_Bot;
 
 namespace FantamortoBot
 {
 	class FantamortoBot
 	{
-		private static readonly string filePath = @"lists\";
-
-		private static readonly Dictionary<string, Action<MessageEventArgs>> commandsMap = new Dictionary<string, Action<MessageEventArgs>>(StringComparer.InvariantCultureIgnoreCase)
-		{
-			["/new"] = NewList,
-			["/list"] = PrintList,
-			["/add"] = AddName,
-			["/remove"] = RemoveName,
-			["/death"] = NewDeath
-		};
+		public static readonly string filePath = @"lists\";
 
 		static ITelegramBotClient botClient;
 
@@ -47,100 +39,24 @@ namespace FantamortoBot
 			{
 				Console.WriteLine($"Received message: {e.Message.Text} from user: {e.Message.From.FirstName}");
 
+				var ch = new CommandHandler(filePath, e);
+				var commandsMap = new Dictionary<string, Action>(StringComparer.InvariantCultureIgnoreCase)
+				{
+					["/new"] = ch.NewList,
+					["/list"] = ch.PrintList,
+					["/add"] = ch.AddName,
+					["/remove"] = ch.RemoveName,
+					["/dead"] = ch.AddDead
+				};
+
 				var command = e.Message.Text.Split(" ")[0];
 
 				if (commandsMap.ContainsKey(command))
-					commandsMap[command](e);
-			}
-		}
-
-		private static bool ListExists(MessageEventArgs e)
-		{
-			string fileName = filePath + e.Message.From.Id.ToString();
-			string response;
-
-			if (File.Exists(fileName))
-			{
-				return true;
-			}
-			else
-			{
-				response = $"No list for {e.Message.From.FirstName} was found, use /new to create a new list";
-				botClient.SendTextMessageAsync(e.Message.Chat, response);
-			}
-
-			return false;
-		}
-
-		private static async void NewList(MessageEventArgs e)
-		{
-			string fileName = filePath + e.Message.From.Id.ToString();
-			string response;
-
-			if (File.Exists(fileName))
-			{
-				response = $"List for {e.Message.From.Id} already exists";
-				PrintList(e);
-			}
-			else
-			{
-				response = $"Created new list for {e.Message.From.FirstName}, use /add to add names";
-				using FileStream fs = File.Create(fileName);
-			}
-
-			await botClient.SendTextMessageAsync(e.Message.Chat, response);
-		}
-
-		private static async void PrintList(MessageEventArgs e)
-		{
-			string fileName = filePath + e.Message.From.Id.ToString();
-			string response;
-
-			if (ListExists(e))
-			{
-				response = await File.ReadAllTextAsync(fileName);
-
-				if (response == "")
 				{
-					response = "Empty list, use /add to add names";
+					commandsMap[command]();
+					await botClient.SendTextMessageAsync(e.Message.Chat, ch.GetResponse());
 				}
-
-				await botClient.SendTextMessageAsync(e.Message.Chat, response);
 			}
-		}
-
-		private static async void AddName(MessageEventArgs e)
-		{
-			string fileName = filePath + e.Message.From.Id.ToString();
-			var args = e.Message.Text.Split(" ", 2);
-			string response;
-
-			if (ListExists(e))
-			{
-				if (args.Length != 2)
-				{
-					response = "Argument missing, use /add <name>";
-				}
-				else
-				{
-					await File.AppendAllTextAsync(fileName, $"{args[1]}\n");
-					response = $"Added {args[1]} to {fileName}";
-				}
-				
-				await botClient.SendTextMessageAsync(e.Message.Chat, response);
-			}
-		}
-
-		private static async void RemoveName(MessageEventArgs e)
-		{
-			string response = "Command not implemented yet";
-			await botClient.SendTextMessageAsync(e.Message.Chat, response);
-		}
-
-		private static async void NewDeath(MessageEventArgs e)
-		{
-			string response = "Command not implemented yet";
-			await botClient.SendTextMessageAsync(e.Message.Chat, response);
 		}
 	}
 }
